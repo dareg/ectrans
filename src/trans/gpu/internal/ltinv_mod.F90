b01b9eb6f8173b49e52bac1080589c6e9cde7169
Harold Petithomme <harold.petithomme@meteo.fr>
2023-11-13 18:35:47 +0000

    Remove some dead code, reorganize a bit, check set_resol
    use offset instead of array section

--- src/trans/gpu/internal/ltinv_mod.F90
+++ src/trans/gpu/internal/ltinv_mod.F90
@@ -1,5 +1,5 @@
 ! (C) Copyright 2000- ECMWF.
-! 
+!
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 ! In applying this licence, ECMWF does not waive the privileges and immunities
@@ -8,44 +8,39 @@
 !
 
 MODULE LTINV_MOD
-  CONTAINS
-  SUBROUTINE LTINV(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,KLEI2,KDIM1,&
-   & PSPVOR,PSPDIV,PSPSCALAR,&
-   & PSPSC3A,PSPSC3B,PSPSC2 , &
-   & KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
-  
-  USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRB,   JPRBT
-  USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
-  
-  USE TPM_DIM         ,ONLY : R
+  USE PARKIND1  ,ONLY : JPIM     ,JPRB
+  USE PARKIND_ECTRANS  ,ONLY : JPRBT
+  USE TPM_DIM         ,ONLY : R,R_NSMAX, R_NDGNH, R_NDGL
+  USE TPM_DISTR       ,ONLY : D,D_NUMP,D_MYMS,D_NASM0,D_NSTAGT0B,D_NSTAGT1B,D_NPROCL,D_NPNTGTB1
   USE TPM_TRANS       ,ONLY : LDIVGP, LVORGP, NF_SC2, NF_SC3A, NF_SC3B, foubuf_in
-  USE TPM_FLT
-  USE TPM_GEOMETRY
-  USE TPM_DISTR       ,ONLY : D
-  use tpm_gen, only: nout
-  USE PREPSNM_MOD     ,ONLY : PREPSNM
-  USE PRFI1B_MOD       ,ONLY : PRFI1B
+  USE TPM_GEOMETRY    ,ONLY : G, G_NDGLU
+  USE TPM_FIELDS      ,ONLY : F,ZIA,ZSOA1,ZAOA1,ZEPSNM
+  USE YOMHOOK,ONLY : LHOOK, DR_HOOK, JPHOOK
+
+  IMPLICIT NONE
+
+CONTAINS
+SUBROUTINE LTINV(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,KLEI2,KDIM1,&
+ & PSPVOR,PSPDIV,PSPSCALAR,PSPSC3A,PSPSC3B,PSPSC2,KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
+  !USE TPM_FLT
+  !USE TPM_GEOMETRY
   USE VDTUV_MOD       ,ONLY : VDTUV
   USE SPNSDE_MOD      ,ONLY : SPNSDE
   USE LEINV_MOD       ,ONLY : LEINV
-  USE ASRE1B_MOD      ,ONLY : ASRE1B
   USE FSPGL_INT_MOD   ,ONLY : FSPGL_INT
   USE ABORT_TRANS_MOD ,ONLY : ABORT_TRANS
-  use ieee_arithmetic
-  USE TPM_FIELDS      ,ONLY : F,ZIA,ZSOA1,ZAOA1,ZEPSNM
-  
-  
+
   !**** *LTINV* - Inverse Legendre transform
   !
   !     Purpose.
   !     --------
   !        Tranform from Laplace space to Fourier space, compute U and V
   !        and north/south derivatives of state variables.
-  
+
   !**   Interface.
   !     ----------
   !        *CALL* *LTINV(...)
-  
+
   !        Explicit arguments :
   !        --------------------
   !          KM        - zonal wavenumber
@@ -53,46 +48,43 @@ MODULE LTINV_MOD
   !          PSPVOR    - spectral vorticity
   !          PSPDIV    - spectral divergence
   !          PSPSCALAR - spectral scalar variables
-  
+
   !        Implicit arguments :  The Laplace arrays of the model.
   !        --------------------  The values of the Legendre polynomials
   !                              The grid point arrays of the model
   !     Method.
   !     -------
-  
+
   !     Externals.
   !     ----------
-  
+
   !         PREPSNM - prepare REPSNM for wavenumber KM
   !         PRFI1B  - prepares the spectral fields
   !         VDTUV   - compute u and v from vorticity and divergence
   !         SPNSDE  - compute north-south derivatives
   !         LEINV   - Inverse Legendre transform
   !         ASRE1   - recombination of symmetric/antisymmetric part
-  
+
   !     Reference.
   !     ----------
   !        ECMWF Research Department documentation of the IFS
   !        Temperton, 1991, MWR 119 p1303
-  
+
   !     Author.
   !     -------
   !        Mats Hamrud  *ECMWF*
-  
+
   !     Modifications.
   !     --------------
   !        Original : 00-02-01 From LTINV in IFS CY22R1
   !     ------------------------------------------------------------------
-  
-  IMPLICIT NONE
-  
+
   INTEGER(KIND=JPIM), INTENT(IN) :: KF_OUT_LT
   INTEGER(KIND=JPIM), INTENT(IN) :: KF_UV
   INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCALARS
   INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCDERS
   INTEGER(KIND=JPIM), INTENT(IN) :: KLEI2
   INTEGER(KIND=JPIM), INTENT(IN) :: KDIM1
-  
   REAL(KIND=JPRB)   ,OPTIONAL,INTENT(IN)  :: PSPVOR(:,:)
   REAL(KIND=JPRB)   ,OPTIONAL,INTENT(IN)  :: PSPDIV(:,:)
   REAL(KIND=JPRB)   ,OPTIONAL,INTENT(IN)  :: PSPSCALAR(:,:)
@@ -101,28 +93,31 @@ MODULE LTINV_MOD
   REAL(KIND=JPRB)   ,OPTIONAL,INTENT(IN)  :: PSPSC3B(:,:,:)
   INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN)  :: KFLDPTRUV(:)
   INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN)  :: KFLDPTRSC(:)
-  
+
   EXTERNAL  FSPGL_PROC
   OPTIONAL  FSPGL_PROC
-  
+
   INTEGER(KIND=JPIM) :: IFC, ISTA, IIFC, IDGLU
   INTEGER(KIND=JPIM) :: IVORL,IVORU,IDIVL,IDIVU,IUL,IUU,IVL,IVU,ISL,ISU,IDL,IDU
   INTEGER(KIND=JPIM) :: IFIRST, ILAST, IDIM1,IDIM2,IDIM3,J3
-  INTEGER(KIND=JPIM) :: KM
-  INTEGER(KIND=JPIM) :: KMLOC
+  INTEGER(KIND=JPIM) :: KM,KMLOC
   REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
-  
+
+  ! added
+  IF (PRESENT(KFLDPTRUV)) call abort_trans("Error: kfldptruv not supported in GPU version")
+  IF (PRESENT(KFLDPTRSC)) call abort_trans("Error: kfldptrsc not supported in GPU version")
+
   IF (LHOOK) CALL DR_HOOK('LTINV_MOD',0,ZHOOK_HANDLE)
-  
-  !     ------------------------------------------------------------------
-  
-  
+
   !*       3.    SPECTRAL COMPUTATIONS FOR U,V AND DERIVATIVES.
   !              ----------------------------------------------
-  
+
+  !ILEI2 = 2*(4*KF_UV+KF_SCALARS+KF_SCDERS)
+  !IDIM1 = 2*KF_OUT_LT
+
   IFIRST = 1
   ILAST  = 0
-  
+
   !*       1.    PREPARE ZEPSNM.
   !              ---------------
 
@@ -135,26 +130,32 @@ MODULE LTINV_MOD
     IUU   = 6*KF_UV
     IVL   = 6*KF_UV+1
     IVU   = 8*KF_UV
-  
+
     IDIM2=UBOUND(PSPVOR,2)
+
     !$ACC DATA COPYIN(PSPVOR,PSPDIV)
-    CALL PRFI1B(ZIA(IVORL:IVORU,:,:),PSPVOR,KF_UV,IDIM2,KFLDPTRUV)
-    CALL PRFI1B(ZIA(IDIVL:IDIVU,:,:),PSPDIV,KF_UV,IDIM2,KFLDPTRUV)
+
+    !CALL PRFI1B(ZIA,PSPVOR,KF_UV,IDIM2,KFLDPTRUV,ivorl-1)
+    !CALL PRFI1B(ZIA,PSPDIV,KF_UV,IDIM2,KFLDPTRUV,idivl-1)
+    CALL PRFI1A(ZIA,PSPVOR,KF_UV,IDIM2,ivorl-1)
+    CALL PRFI1A(ZIA,PSPDIV,KF_UV,IDIM2,idivl-1)
+
     !$ACC END DATA
-  
+
     CALL VDTUV(KF_UV,ZEPSNM,ZIA(IVORL:IVORU,:,:),ZIA(IDIVL:IDIVU,:,:),&
      & ZIA(IUL:IUU,:,:),ZIA(IVL:IVU,:,:))
     ILAST = ILAST+8*KF_UV
   ENDIF
-  
+
   IF(KF_SCALARS > 0)THEN
     IF(PRESENT(PSPSCALAR)) THEN
       IFIRST = ILAST+1
       ILAST  = IFIRST-1+2*KF_SCALARS
-  
+
       IDIM2=UBOUND(PSPSCALAR,2)
       !$ACC DATA COPYIN(PSPSCALAR)
-      CALL PRFI1B(ZIA(IFIRST:ILAST,:,:),PSPSCALAR,KF_SCALARS,IDIM2,KFLDPTRSC)
+      !CALL PRFI1B(ZIA,PSPSCALAR,KF_SCALARS,IDIM2,KFLDPTRSC,ifirst-1)
+      CALL PRFI1A(ZIA,PSPSCALAR,KF_SCALARS,IDIM2,ifirst-1)
       !$ACC END DATA
     ELSE
       IF(PRESENT(PSPSC2) .AND. NF_SC2 > 0) THEN
@@ -162,7 +163,7 @@ MODULE LTINV_MOD
         ILAST  = IFIRST-1+2*NF_SC2
         IDIM2=UBOUND(PSPSC2,2)
         !$ACC DATA COPYIN(PSPSC2)
-        CALL PRFI1A(ZIA(IFIRST:ILAST,:,:),PSPSC2,NF_SC2,IDIM2)
+        CALL PRFI1A(ZIA,PSPSC2,NF_SC2,IDIM2,ifirst-1)
         !$ACC END DATA
       ENDIF
       IF(PRESENT(PSPSC3A) .AND. NF_SC3A > 0) THEN
@@ -174,7 +175,7 @@ MODULE LTINV_MOD
           IFIRST = ILAST+1
           ILAST  = IFIRST-1+2*IDIM1
 
-          CALL PRFI1A(ZIA(IFIRST:ILAST,:,:),PSPSC3A(:,:,J3),IDIM1,IDIM2)
+          CALL PRFI1A(ZIA,PSPSC3A(:,:,J3),IDIM1,IDIM2,ifirst-1)
         ENDDO
         !$ACC END DATA
       ENDIF
@@ -186,8 +187,8 @@ MODULE LTINV_MOD
         DO J3=1,IDIM3
           IFIRST = ILAST+1
           ILAST  = IFIRST-1+2*IDIM1
-  
-          CALL PRFI1A(ZIA(IFIRST:ILAST,:,:),PSPSC3B(:,:,J3),IDIM1,IDIM2)
+
+          CALL PRFI1A(ZIA,PSPSC3B(:,:,J3),IDIM1,IDIM2,ifirst-1)
         ENDDO
         !$ACC END DATA
       ENDIF
@@ -198,7 +199,7 @@ MODULE LTINV_MOD
       CALL ABORT_TRANS('LTINV_MOD:ILAST /= 8*KF_UV+2*KF_SCALARS')
     ENDIF
   ENDIF
-  
+
   IF (KF_SCDERS > 0) THEN
      ! stop 'Error: code path not (yet) supported in GPU version'
      ISL = 2*(4*KF_UV)+1
@@ -208,7 +209,7 @@ MODULE LTINV_MOD
      write(0,*) "ltinv ders (spnsde) - inds:",isl,isu,idl,idu
      CALL SPNSDE(KF_SCALARS,ZEPSNM,ZIA(ISL:ISU,:,:),ZIA(IDL:IDU,:,:))
   ENDIF
-  
+
   !*       4.    INVERSE LEGENDRE TRANSFORM.
   !              ---------------------------
 
@@ -222,7 +223,7 @@ MODULE LTINV_MOD
 
     ! RECOMBINATION SYMMETRIC/ANTISYMMETRIC PART.
     CALL ASRE1B(KF_OUT_LT,ZAOA1,ZSOA1)
-  
+
     ! OPTIONAL COMPUTATIONS IN FOURIER SPACE
     IF(PRESENT(FSPGL_PROC)) THEN
       stop 'Error: SPGL_PROC is not (yet) optimized in GPU version'
@@ -233,32 +234,26 @@ MODULE LTINV_MOD
   IF (LHOOK) CALL DR_HOOK('LTINV_MOD',1,ZHOOK_HANDLE)
 END SUBROUTINE LTINV
 
-SUBROUTINE PRFI1A(PIA,PSPEC,KFIELDS,KDIM)
-  USE PARKIND1  ,ONLY : JPIM     ,JPRB
-  USE TPM_DIM         ,ONLY : R,R_NSMAX
-  USE TPM_DISTR       ,ONLY : D,D_NUMP,D_MYMS,D_NASM0
-  use ieee_arithmetic
-
+SUBROUTINE PRFI1A(PIA,PSPEC,KFIELDS,KDIM,ioff)
   INTEGER(KIND=JPIM),INTENT(IN)   :: KFIELDS
   REAL(KIND=JPRB)   ,INTENT(IN)   :: PSPEC(:,:)
-  REAL(KIND=JPRB)   ,INTENT(INOUT)  :: PIA(:,:,:)
-  INTEGER(KIND=JPIM),INTENT(IN) :: KDIM
+  REAL(KIND=JPRB)   ,INTENT(OUT)  :: PIA(:,:,:)
+  INTEGER(KIND=JPIM),INTENT(IN) :: KDIM,ioff
 
-  INTEGER(KIND=JPIM) :: KM,KMLOC,INM, J, JFLD, ILCM
+  INTEGER(KIND=JPIM) :: KM,KMLOC,INM, JN, JFLD
 
-  !$ACC DATA PRESENT(D_NUMP,R_NSMAX,D_MYMS,D_NASM0,PIA,PSPEC)
+  !$ACC DATA copyin(ioff) PRESENT(D_NUMP,R_NSMAX,D_MYMS,D_NASM0,PIA,PSPEC)
 
-  !$ACC PARALLEL LOOP COLLAPSE(3) PRIVATE(KM,ILCM,INM)
+  !$ACC PARALLEL LOOP COLLAPSE(3) PRIVATE(KM,INM)
   DO KMLOC=1,D_NUMP
-     DO J=1,R_NSMAX+1
+     DO JN=0,R_NSMAX
         DO JFLD=1,KFIELDS
            KM = D_MYMS(KMLOC)
-           ILCM = R_NSMAX+1-KM
-           if (J <= ILCM) then
-              INM = D_NASM0(KM)+2*(ILCM-J)
+           if (JN <= R_NSMAX-KM) then
+              INM = D_NASM0(KM)+2*(R_NSMAX-KM-JN)
               IF( INM < KDIM ) THEN
-              PIA(2*JFLD-1,J+2,KMLOC) = PSPEC(JFLD,INM  )
-              PIA(2*JFLD,J+2,KMLOC) = PSPEC(JFLD,INM+1)
+              PIA(ioff+2*JFLD-1,JN+3,KMLOC) = PSPEC(JFLD,INM)
+              PIA(ioff+2*JFLD,JN+3,KMLOC) = PSPEC(JFLD,INM+1)
               ENDIF
            end if
         ENDDO
@@ -267,7 +262,7 @@ SUBROUTINE PRFI1A(PIA,PSPEC,KFIELDS,KDIM)
 
   !$ACC PARALLEL LOOP COLLAPSE(2) PRIVATE(KM)
   DO KMLOC=1,D_NUMP
-     DO JFLD=1,2*KFIELDS
+     DO JFLD=ioff+1,ioff+2*KFIELDS
         KM = D_MYMS(KMLOC)
         PIA(JFLD,1,KMLOC) = 0.0_JPRB
         PIA(JFLD,2,KMLOC) = 0.0_JPRB
@@ -277,5 +272,70 @@ SUBROUTINE PRFI1A(PIA,PSPEC,KFIELDS,KDIM)
 
   !$ACC END DATA
 END SUBROUTINE
+
+SUBROUTINE PRFI1B(PIA,PSPEC,KFIELDS,KDIM,KFLDPTR,ioff)
+   INTEGER(KIND=JPIM),INTENT(IN) :: KFIELDS
+   REAL(KIND=JPRB)   ,INTENT(IN)   :: PSPEC(:,:)
+   REAL(KIND=JPRB)   ,INTENT(OUT)  :: PIA(:,:,:)
+   INTEGER(KIND=JPIM),INTENT(IN) :: KDIM,ioff
+   INTEGER(KIND=JPIM),INTENT(IN) :: KFLDPTR(:)
+
+   INTEGER(KIND=JPIM) :: KM,KMLOC,INM, JN, JFLD
+
+   !$ACC DATA copyin(ioff) PRESENT(D_NUMP,R_NSMAX,D_MYMS,D_NASM0,PIA,PSPEC,KFLDPTR)
+
+   !$ACC PARALLEL LOOP COLLAPSE(3) DEFAULT(NONE) PRIVATE(KM,INM)
+   DO KMLOC=1,D_NUMP
+      DO JN=0,R_NSMAX
+         DO JFLD=1,KFIELDS
+            KM = D_MYMS(KMLOC)
+            IF (JN <= R_NSMAX-KM) THEN
+               INM = D_NASM0(KM)+2*(R_NSMAX-KM-JN)
+               PIA(ioff+2*JFLD-1,JN+3,KMLOC) = PSPEC(KFLDPTR(JFLD),INM)
+               PIA(ioff+2*JFLD,JN+3,KMLOC) = PSPEC(KFLDPTR(JFLD),INM+1)
+            END IF
+         ENDDO
+      ENDDO
+   END DO
+
+   !$ACC PARALLEL LOOP DEFAULT(NONE) COLLAPSE(2) PRIVATE(KM)
+   DO KMLOC=1,D_NUMP
+      DO JFLD=ioff+1,ioff+2*KFIELDS
+         KM = D_MYMS(KMLOC)
+         PIA(JFLD,1,KMLOC) = 0.0_JPRB
+         PIA(JFLD,2,KMLOC) = 0.0_JPRB
+         PIA(JFLD,R_NSMAX+4-KM,KMLOC) = 0.0_JPRB
+      ENDDO
+   END DO
+
+   !$ACC END DATA
+END SUBROUTINE
+
+SUBROUTINE ASRE1B(KFIELD,PAOA,PSOA)
+   INTEGER(KIND=JPIM), INTENT(IN) :: KFIELD
+   REAL(KIND=JPRBT),   INTENT(IN)  :: PSOA(:,:,:)
+   REAL(KIND=JPRBT),   INTENT(IN)  :: PAOA(:,:,:)
+
+   INTEGER(KIND=JPIM) :: KM,KMLOC,ISL, IGLS, JFLD, JGL , ISTAN, ISTAS
+
+   !$ACC DATA PRESENT(PAOA,PSOA,D_MYMS,D_NPROCL,D_NSTAGT0B,D_NPNTGTB1,G_NDGLU,FOUBUF_IN)
+
+   !$ACC PARALLEL LOOP DEFAULT(NONE) PRIVATE(KM,ISL,JGL,JFLD,ISTAN,IGLS,ISTAS)
+   DO KMLOC=1,D_NUMP
+      KM = D_MYMS(KMLOC)
+      ISL = MAX(R_NDGNH-G_NDGLU(KM)+1,1)
+      DO JGL=ISL, R_NDGNH
+         ISTAN = (D_NSTAGT0B(D_NPROCL(JGL)) + D_NPNTGTB1(KMLOC,JGL))*2*KFIELD
+         IGLS = R_NDGL+1-JGL
+         ISTAS = (D_NSTAGT0B(D_NPROCL(IGLS)) + D_NPNTGTB1(KMLOC,IGLS))*2*KFIELD
+         DO JFLD=1,2*KFIELD
+            FOUBUF_IN(ISTAN+JFLD) = PSOA(JFLD,JGL,KMLOC)+PAOA(JFLD,JGL,KMLOC)
+            FOUBUF_IN(ISTAS+JFLD) = PSOA(JFLD,JGL,KMLOC)-PAOA(JFLD,JGL,KMLOC)
+         ENDDO
+      ENDDO
+   ENDDO
+
+   !$ACC END DATA
+END SUBROUTINE
 END MODULE LTINV_MOD
-  
+
--- src/trans/gpu/internal/set_resol_mod.F90
+++ src/trans/gpu/internal/set_resol_mod.F90
@@ -13,10 +13,11 @@ SUBROUTINE SET_RESOL(KRESOL,LDSETUP)
 USE PARKIND1  ,ONLY : JPIM
 
 USE TPM_GEN         ,ONLY : NOUT, MSETUP0, NCUR_RESOL, NMAX_RESOL,LENABLED
-USE TPM_DIM         ,ONLY : R, DIM_RESOL
+USE TPM_DIM         ,ONLY : R, DIM_RESOL,r_nsmax,r_ntmax,r_ndgl,r_ndgnh
 !USE TPM_TRANS
-USE TPM_DISTR       ,ONLY : D, DISTR_RESOL
-USE TPM_GEOMETRY    ,ONLY : G, GEOM_RESOL
+USE TPM_DISTR       ,ONLY : D,DISTR_RESOL,d_nump,d_myms,d_nasm0,d_nptrls,d_nprocl,&
+  d_nprocm,d_mstabf,d_nstagtf,d_nstagt0b,d_nstagt1b,d_npntgtb0,d_npntgtb1
+USE TPM_GEOMETRY    ,ONLY : G,GEOM_RESOL,g_nloen,g_ndglu,g_nmen
 USE TPM_FIELDS      ,ONLY : F, FIELDS_RESOL
 USE TPM_FFT         ,ONLY : T, FFT_RESOL
 #ifdef WITH_FFTW
@@ -41,36 +42,64 @@ LOGICAL :: LLSETUP
 
 !     ------------------------------------------------------------------
 
-IF(MSETUP0 == 0) CALL ABORT_TRANS('SET_RESOL:TRANS NOT SETUP')
+IF (MSETUP0 == 0) CALL ABORT_TRANS('TRANS NOT SETUP')
+
 LLSETUP = .FALSE.
 IF(PRESENT(LDSETUP)) LLSETUP = LDSETUP
+
 IRESOL = 1
-IF(PRESENT(KRESOL)) THEN
-  IRESOL = KRESOL
- IF(IRESOL < 1 .OR. IRESOL > NMAX_RESOL) THEN
-   WRITE(NOUT,*)'SET_RESOL: UNKNOWN RESOLUTION ',IRESOL,NMAX_RESOL
-   CALL ABORT_TRANS('SET_RESOL:IRESOL < 1 .OR. KRESOL > NMAX_RESOL')
+IF (PRESENT(KRESOL)) THEN
+  IF(KRESOL < 1 .OR. KRESOL > NMAX_RESOL) THEN
+    CALL ABORT_TRANS('RESOL < 1 .OR. RESOL > NMAX_RESOL')
   ENDIF
-  IF(.NOT.LLSETUP) THEN
-    IF(.NOT.LENABLED(IRESOL)) THEN
-      WRITE(NOUT,*)'SET_RESOL: UNKNOWN RESOLUTION ',IRESOL,LENABLED
-      CALL ABORT_TRANS('SET_RESOL:IRESOL NOT ENABLED')
-     ENDIF
-   ENDIF
+
+  IRESOL = KRESOL
 ENDIF
-IF(IRESOL /= NCUR_RESOL) THEN
+
+IF(.NOT.(LLSETUP.or.LENABLED(IRESOL))) CALL ABORT_TRANS('IRESOL NOT ENABLED OUT OF SETUP')
+
+IF (IRESOL /= NCUR_RESOL) THEN
+  write(nout,*) "--> changing resolution from/to:",ncur_resol,iresol
   NCUR_RESOL = IRESOL
   R => DIM_RESOL(NCUR_RESOL)
   F => FIELDS_RESOL(NCUR_RESOL)
   G => GEOM_RESOL(NCUR_RESOL)
   D => DISTR_RESOL(NCUR_RESOL)
   T => FFT_RESOL(NCUR_RESOL)
-#ifdef WITH_FFTW
-  TW => FFTW_RESOL(NCUR_RESOL)
-#endif
   TC => FFTC_RESOL(NCUR_RESOL)
   S => FLT_RESOL(NCUR_RESOL)
   C => CTL_RESOL(NCUR_RESOL)
+#ifdef WITH_FFTW
+  TW => FFTW_RESOL(NCUR_RESOL)
+#endif
+
+  if (.not.llsetup) then
+	 r_nsmax = -1
+	 !$acc update self(r_nsmax,r_ntmax,r_ndgl,r_ndgnh)
+	 if (r%nsmax /= r_nsmax) call abort_trans("Error: r_nsmax")
+	 if (r%ntmax /= r_ntmax) call abort_trans("Error: r_ntmax")
+	 if (r%ndgl /= r_ndgl) call abort_trans("Error: r_ndgl")
+	 if (r%ndgnh /= r_ndgnh) call abort_trans("Error: r_ndgnh")
+	 !$acc update self(g_nloen,g_ndglu,g_nmen)
+	 if (any(g%nloen /= g_nloen)) call abort_trans("Error: g_nloen")
+	 if (any(g%ndglu /= g_ndglu)) call abort_trans("Error: g_ndglu")
+	 if (any(g%nmen /= g_nmen)) call abort_trans("Error: g_nmen")
+
+	 !$acc update self(d_nump,d_myms,d_nasm0,d_nptrls,d_nprocl,d_nprocm,d_mstabf,d_nstagtf,&
+	 !$acc d_nstagt0b,d_nstagt1b,d_npntgtb0,d_npntgtb1)
+	 if (d%nump /= d_nump) call abort_trans("Error: d_nump")
+	 if (any(d%myms /= d_myms)) call abort_trans("Error: d_myms")
+	 if (any(d%nasm0 /= d_nasm0)) call abort_trans("Error: d_nasm0")
+	 if (any(d%nptrls /= d_nptrls)) call abort_trans("Error: d_nptrls")
+	 if (any(d%nprocl /= d_nprocl)) call abort_trans("Error: d_nprocl")
+	 if (any(d%nprocm /= d_nprocm)) call abort_trans("Error: d_nprocm")
+	 if (any(d%mstabf /= d_mstabf)) call abort_trans("Error: d_mstabf")
+	 if (any(d%nstagtf /= d_nstagtf)) call abort_trans("Error: d_nstagtf")
+	 if (any(d%nstagt0b /= d_nstagt0b)) call abort_trans("Error: d_nstagt0b")
+	 if (any(d%nstagt1b /= d_nstagt1b)) call abort_trans("Error: d_nstagt1b")
+	 if (any(d%npntgtb0 /= d_npntgtb0)) call abort_trans("Error: d_npntgtb0")
+	 if (any(d%npntgtb1 /= d_npntgtb1)) call abort_trans("Error: d_npntgtb1")
+  end if
 ENDIF
 
 END SUBROUTINE SET_RESOL
